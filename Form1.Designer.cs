a5aa1bd12a4b2211399ea09171dfeb9d54a9e6c9
sinemkonceli <56769023+sinemkonceli@users.noreply.github.com>
2020-12-09 00:25:39 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace _4.Soru
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.lstSayi = new System.Windows.Forms.ListBox();
+            this.btn_Ekle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblToplam = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblOrt = new System.Windows.Forms.Label();
+            this.lblEnBuyuk = new System.Windows.Forms.Label();
+            this.lblEnKucuk = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSayi.Location = new System.Drawing.Point(15, 63);
+            this.txtSayi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(132, 29);
+            this.txtSayi.TabIndex = 0;
+            // 
+            // lstSayi
+            // 
+            this.lstSayi.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lstSayi.FormattingEnabled = true;
+            this.lstSayi.ItemHeight = 21;
+            this.lstSayi.Location = new System.Drawing.Point(156, 26);
+            this.lstSayi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstSayi.Name = "lstSayi";
+            this.lstSayi.Size = new System.Drawing.Size(159, 172);
+            this.lstSayi.TabIndex = 1;
+            // 
+            // btn_Ekle
+            // 
+            this.btn_Ekle.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_Ekle.Location = new System.Drawing.Point(33, 101);
+            this.btn_Ekle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Ekle.Name = "btn_Ekle";
+            this.btn_Ekle.Size = new System.Drawing.Size(100, 28);
+            this.btn_Ekle.TabIndex = 2;
+            this.btn_Ekle.Text = "EKLE";
+            this.btn_Ekle.UseVisualStyleBackColor = true;
+            this.btn_Ekle.Click += new System.EventHandler(this.btn_Ekle_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(341, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Toplam:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(325, 144);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "En Küçük:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(325, 73);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ortalama:";
+            // 
+            // lblToplam
+            // 
+            this.lblToplam.AutoSize = true;
+            this.lblToplam.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblToplam.Location = new System.Drawing.Point(435, 33);
+            this.lblToplam.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblToplam.Name = "lblToplam";
+            this.lblToplam.Size = new System.Drawing.Size(19, 23);
+            this.lblToplam.TabIndex = 7;
+            this.lblToplam.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(324, 108);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 23);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "En büyük:";
+            // 
+            // lblOrt
+            // 
+            this.lblOrt.AutoSize = true;
+            this.lblOrt.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblOrt.Location = new System.Drawing.Point(435, 73);
+            this.lblOrt.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblOrt.Name = "lblOrt";
+            this.lblOrt.Size = new System.Drawing.Size(19, 23);
+            this.lblOrt.TabIndex = 9;
+            this.lblOrt.Text = "0";
+            // 
+            // lblEnBuyuk
+            // 
+            this.lblEnBuyuk.AutoSize = true;
+            this.lblEnBuyuk.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEnBuyuk.Location = new System.Drawing.Point(435, 108);
+            this.lblEnBuyuk.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEnBuyuk.Name = "lblEnBuyuk";
+            this.lblEnBuyuk.Size = new System.Drawing.Size(19, 23);
+            this.lblEnBuyuk.TabIndex = 10;
+            this.lblEnBuyuk.Text = "0";
+            // 
+            // lblEnKucuk
+            // 
+            this.lblEnKucuk.AutoSize = true;
+            this.lblEnKucuk.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEnKucuk.Location = new System.Drawing.Point(435, 144);
+            this.lblEnKucuk.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEnKucuk.Name = "lblEnKucuk";
+            this.lblEnKucuk.Size = new System.Drawing.Size(19, 23);
+            this.lblEnKucuk.TabIndex = 11;
+            this.lblEnKucuk.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 303);
+            this.Controls.Add(this.lblEnKucuk);
+            this.Controls.Add(this.lblEnBuyuk);
+            this.Controls.Add(this.lblOrt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblToplam);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Ekle);
+            this.Controls.Add(this.lstSayi);
+            this.Controls.Add(this.txtSayi);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSayi;
+        private System.Windows.Forms.ListBox lstSayi;
+        private System.Windows.Forms.Button btn_Ekle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblToplam;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblOrt;
+        private System.Windows.Forms.Label lblEnBuyuk;
+        private System.Windows.Forms.Label lblEnKucuk;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4.Soru
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        int sayac;
+        int sayi;
+        int toplam;
+        int buyuk=int.MinValue;
+        int kucuk = int.MaxValue;
+        double ort;
+
+        private void btn_Ekle_Click(object sender, EventArgs e)
+        {
+            sayac++;
+            int sayi = Convert.ToInt32(txtSayi.Text);
+            lstSayi.Items.Add(sayac + " .Sayi: " + sayi);
+            toplam += sayi;
+            lblToplam.Text = toplam.ToString();
+            ort = toplam / lstSayi.Items.Count;
+            lblOrt.Text = ort.ToString();
+            if (buyuk < sayi)
+            {
+                buyuk = sayi;
+            }
+            if (kucuk > sayi)
+            {
+                kucuk = sayi;
+            }           
+            txtSayi.Clear();
+            lblEnKucuk.Text = kucuk.ToString();
+            lblEnBuyuk.Text = buyuk.ToString();        
+        }
+    }
+}
